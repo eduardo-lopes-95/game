fcb4adc7bf08a71bf9f88b1e4dbc2dbc207225b3
Eduardo Sabino <eduardosabino@ymail.com>
2022-06-09 23:24:12 -0300

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+enum VisitorAction{
+    Accept,
+    AcceptWithNote {note:String},
+    Refuse,
+    Probation,
+}
+
+#[derive(Debug)]
 struct Visitor{
     name:String,
-    greeting: String,
+    action: VisitorAction,
+    age: i8
 }
 
 impl Visitor {
     //Constructor
-    fn new(name: &str, greeting: &str) -> Self{
-        Self { name: name.to_lowercase(), greeting: greeting.to_string() }
+    // fn new(name: &str, greeting: &str) -> Self{
+    //     Self { name: name.to_lowercase(), greeting: greeting.to_string() }
+    // }
+
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self{
+        Self { name: name.to_string(), action, age}
     }
     
     //Associated function = method
+    // fn greet_visitor(&self){
+    //     println!("{}", self.greeting)
+    // }
+
     fn greet_visitor(&self){
-        println!("{}", self.greeting)
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("Note: , {}", note);
+                if self.age < 21{
+                    println!("Do not serve alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a
+            probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
     }
+
 }
 
 fn friend_on_the_list(name: &str) -> bool{
@@ -34,23 +65,45 @@ fn qual_seu_nome() -> String{
 }
 
 fn main() {
-    println!("Qual e o seu nome?");
-    
-    let name = qual_seu_nome();
-    
-    let visitor_list = [
-        Visitor::new("Eduardo", "Fala pica"),
-        Visitor::new("Leonardo", "Fala irmão"),
-        Visitor::new("Giselma", "Tudo bem, mamãe?"),
-        Visitor::new("Damaris", "Sr. Sabino"),
+    let mut visitor_list = vec![
+        Visitor::new("Eduardo", VisitorAction::Accept, 26),
+        Visitor::new("Leonardo",VisitorAction::AcceptWithNote 
+            {
+                note: String::from("Pouco alcool")
+            }, 25),
+        Visitor::new("Giselma", VisitorAction::Refuse, 50),
+        Visitor::new("Damaris", VisitorAction::AcceptWithNote 
+            { 
+                note: String::from("coloca vinho")
+            }, 25),
     ];
+
+    loop {
+        println!("Qual e o seu nome?");
+        let name = qual_seu_nome();
+        
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty(){
+                    break;
+                }else{
+                    println!("The name {} isn't on the visitor list", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
+        }
+
+        break;
+    }
     
-    let known_visitor = visitor_list
-        .iter()
-        .find(|visitor| visitor.name == name);
-    
-    match known_visitor {
-        Some(visitor) => visitor.greet_visitor(),
-        None => println!("Sorry pal, your name isn't on the lis. Get out of here!")
-    };
+    println!("The final list of visitors:");
+    println!("{:?}", visitor_list);
+        
 }
+
+//http://media.pragprog.com/titles/hwrust/code/FirstStepsWithRust/treehouse_guestlist_enum/src/main.rs
